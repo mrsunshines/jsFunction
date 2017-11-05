ab9167e303246dba7e2e291569e1c006c2aba2eb
mrsunshines <k.hubertjankowski@gmail.com>
2017-11-05 12:46:54 +0100

    First JS function

---
+++ Gruntfile.js
@@ -0,0 +1,68 @@
+﻿module.exports = function(grunt) {
+
+  grunt.initConfig({
+    sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'css/main.css': 'sass/main.sass'
+        }
+      }
+    },
+    imagemin: {
+      dynamic: {
+        files: [{
+            expand: true,
+            cwd: 'images/',
+            src: ['**/*.{png,jpg,gif}'],
+            dest: 'images/build/'
+        }]
+      }
+    },
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['sass/*.sass'],
+        tasks: ['sass'],
+        options: {
+            spawn: false,
+        }
+    },
+    browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        'css/*.css',
+                        '*.html'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: './Grunt'
+                }
+            }
+        }
+
+     // files: ['<%= jshint.files %>'],
+     // tasks: ['jshint']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-browser-sync');
+  
+  grunt.registerTask('default', ['jshint', 'sass', 'imagemin', 'browserSync', 'watch']);
+
+};
---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+﻿/*function myFirstFunction(parameter) {
+	return 'returnedvalue';
+}
+// wywolanie
+myFirstFunction(); */
+//task
+alert("Welcome to my page that will calculate area covered by a triangle of your choice just type in values and clap your hands while this 'state of the art' computer will perform this super complex operation...by a JS Function!!wow!!")
+
+var	a = prompt ('type in base length'),
+	h = prompt ('type in height'),
+	triangleArea = a*h/2;
+
+function getTriangleArea(a, h) {
+	if ((a <= 0) || (h <= 0)) {
+    alert('Come on .. have you seen a negative distance 4real? Try again with some positive values - Nieprawidłowa wartość - daj cos na plusie');
+    } else if ((a > 0) && (h > 0)) {
+    alert('' + triangleArea + ' jest polem twego trójkąta');	
+	}
+}
+getTriangleArea(a, h);
+/*
+var	a = prompt ('type in base length'),
+	h = prompt ('type in height'),
+	triangleArea = a*h/2;
+if ((a <= 0) || (h <= 0)) {
+    alert('Come on .. have you seen a negative distance 4real? Nieprawidłowa warotść');
+}    
+alert(triangleArea);
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+function getTriangleArea(a, h) {
+	if ((a <= 0) || (h <= 0)) {
+    alert('Come on .. have you seen a negative distance 4real? Nieprawidłowa warotść');
+    } else if ((a > 0) && (h > 0)) {
+    return (a*h/2);	
+	}
+}
+getTriangleArea(a, h);
+// function getTriangleArea(getTriangleArea)
+*/
