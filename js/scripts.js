94d91d23bd11c13cf0a08b5d84c108773aa78338
mrsunshines <k.hubertjankowski@gmail.com>
2017-11-07 02:12:10 +0100

    funcja poprawiona

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,23 @@
-﻿/*function myFirstFunction(parameter) {
-	return 'returnedvalue';
-}
-// wywolanie
-myFirstFunction(); */
-//task
-alert("Welcome to my page that will calculate area covered by a triangle of your choice just type in values and clap your hands while this 'state of the art' computer will perform this super complex operation...by a JS Function!!wow!!")
+﻿alert("Welcome to my page that will calculate area covered by a triangle of your choice just type in values and clap your hands while this 'state of the art' computer will perform this super complex operation...by a JS Function!!wow!!")
 
 var	a = prompt ('type in base length'),
-	h = prompt ('type in height'),
-	triangleArea = a*h/2;
+	h = prompt ('type in height');
 
 function getTriangleArea(a, h) {
 	if ((a <= 0) || (h <= 0)) {
     alert('Come on .. have you seen a negative distance 4real? Try again with some positive values - Nieprawidłowa wartość - daj cos na plusie');
     } else if ((a > 0) && (h > 0)) {
-    alert('' + triangleArea + ' jest polem twego trójkąta');	
+    var triangleArea = a*h/2;	
+    alert('' + triangleArea + ' jest polem twego trójkąta');
 	}
 }
-getTriangleArea(a, h);
-/*
-var	a = prompt ('type in base length'),
-	h = prompt ('type in height'),
-	triangleArea = a*h/2;
-if ((a <= 0) || (h <= 0)) {
-    alert('Come on .. have you seen a negative distance 4real? Nieprawidłowa warotść');
-}    
-alert(triangleArea);
 
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
-function getTriangleArea(a, h) {
-	if ((a <= 0) || (h <= 0)) {
-    alert('Come on .. have you seen a negative distance 4real? Nieprawidłowa warotść');
-    } else if ((a > 0) && (h > 0)) {
-    return (a*h/2);	
-	}
-}
 getTriangleArea(a, h);
-// function getTriangleArea(getTriangleArea)
-*/
+
+var pole1 = getTriangleArea(10, 5);
+var pole2 = getTriangleArea(8, 3);
+var pole3 = getTriangleArea(7, 2);
+
+alert(pole1);
+alert(pole2);
+alert(pole3);
